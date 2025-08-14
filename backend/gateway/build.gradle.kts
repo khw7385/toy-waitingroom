67f636937ec4147352636ef6f396ce5aec853dce
khw7385 <khw7385@naver.com>
2025-08-14 13:08:15 +0900

    feat(gateway): gateway 모듈 초기화

---
+++ backend/gateway/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+}
+
+dependencies {
+    implementation(project(":common-web"))
+    implementation(project(":common-jwt"))
+    implementation(libs.spring.cloud.starter.gateway.server.mvc)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.springdoc.openapi.starter.webmvc.ui)
+}
