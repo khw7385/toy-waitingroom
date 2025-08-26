229fa40fd52abd1f611c0e6e092d37b977d5693f
khw7385 <khw7385@naver.com>
2025-08-26 19:08:01 +0900

    feat: 공통 web 모듈에 swagger 설정 요청 시 토큰을 자동으로 추가

---
+++ backend/common-web/src/main/java/me/khw7385/waitingroom/common/web/config/BaseSwaggerConfig.java
@@ -0,0 +1,26 @@
+package me.khw7385.waitingroom.common.web.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
+public class BaseSwaggerConfig {
+
+    protected OpenAPI buildBaseOpenAPI(){
+        String securitySchemeName = "bearerAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+        Components components = new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .servers(List.of(new Server().url("http://localhost:8080")));
+    }
+}
