94fb41688680aa087ae37f9e3c70cd7157951d4f
khw7385 <khw7385@naver.com>
2025-09-06 14:51:14 +0900

    feat: cors 설정

--- backend/gateway/src/main/java/me/khw7385/waitingroom/gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/me/khw7385/waitingroom/gateway/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package me.khw7385.waitingroom.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +31,7 @@ public class SecurityConfig {
                 .logout(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .requestCache(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
@@ -30,5 +39,20 @@ public class SecurityConfig {
                         auth.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(@Value("${cors.allowed-urls}") List<String> allowedUrls){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(allowedUrls);
+        config.setAllowedMethods(
+                List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name()));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
--- backend/gateway/src/main/java/me/khw7385/waitingroom/gateway/filter/JwtAuthenticationFilter.java
+++ backend/gateway/src/main/java/me/khw7385/waitingroom/gateway/filter/JwtAuthenticationFilter.java
@@ -1,28 +1,53 @@
 package me.khw7385.waitingroom.gateway.filter;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
 import me.khw7385.waitingroom.common.jwt.TokenManager;
 import me.khw7385.waitingroom.common.jwt.dto.TokenMemberClaim;
 import me.khw7385.waitingroom.gateway.exception.TokenRequiredException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.HandlerFilterFunction;
-import org.springframework.web.servlet.function.HandlerFunction;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.util.*;
 
 @Component
-@RequiredArgsConstructor
-public class JwtAuthenticationFilter implements HandlerFilterFunction<ServerResponse, ServerResponse> {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Value("${server.servlet.context-path}")
+    private String SERVLET_CONTEXT_PATH;
+
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String TOKEN_PREFIX = "Bearer ";
     private static final String MEMBER_ID_HEADER = "X-MEMBER-ID";
+    private static final List<String> PERMITTED_URL_PATTERNS = List.of(
+            "/auth/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/*-service/api-docs",
+            "/api-docs/**",
+            "/coupons"
+    );
 
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
     private final TokenManager tokenManager;
 
+    public JwtAuthenticationFilter(TokenManager tokenManager) {
+        this.tokenManager = tokenManager;
+    }
+
     @Override
-    public ServerResponse filter(ServerRequest request, HandlerFunction<ServerResponse> next) throws Exception {
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+
+        return PERMITTED_URL_PATTERNS.stream()
+                .anyMatch(pattern -> pathMatcher.match(pattern, request.getRequestURI()));
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Optional<String> tokenOptional = extractToken(request);
 
         if(tokenOptional.isEmpty()){
@@ -30,16 +55,62 @@ public class JwtAuthenticationFilter implements HandlerFilterFunction<ServerResp
         }
         TokenMemberClaim memberClaim = tokenManager.parseClaims(tokenOptional.get());
 
-        ServerRequest newRequest = ServerRequest.from(request)
-                .header(MEMBER_ID_HEADER, String.valueOf(memberClaim.memberId()))
-                .build();
+        MutableHttpServletRequestWrapper requestWrapper = new MutableHttpServletRequestWrapper(request);
 
-        return next.handle(newRequest);
+        requestWrapper.setHeader(MEMBER_ID_HEADER, String.valueOf(memberClaim.memberId()));
+        filterChain.doFilter(request, response);
     }
 
-    private Optional<String> extractToken(ServerRequest serverRequest){
-        String authorizationValue = serverRequest.headers().firstHeader(AUTHORIZATION_HEADER);
-        if (authorizationValue == null || !authorizationValue.startsWith(TOKEN_PREFIX)) return Optional.empty();
+    private Optional<String> extractToken(HttpServletRequest request){
+        String authorizationValue = request.getHeader(AUTHORIZATION_HEADER);
+        if(authorizationValue == null || !authorizationValue.startsWith(TOKEN_PREFIX)) return Optional.empty();
         return Optional.of(authorizationValue.substring(TOKEN_PREFIX.length()));
+    }
+
+    private static class MutableHttpServletRequestWrapper extends HttpServletRequestWrapper {
+        private HashMap<String, String> customHeaders;
+
+        public MutableHttpServletRequestWrapper(HttpServletRequest request){
+            super(request);
+            this.customHeaders = new HashMap<>();
+        }
+
+        public void setHeader(String headerName, String headerValue){
+            customHeaders.put(headerName, headerValue);
+        }
+
+        @Override
+        public String getHeader(String name) {
+            if(customHeaders.containsKey(name)){
+                return customHeaders.get(name);
+            }
+            return super.getHeader(name);
+        }
+
+        @Override
+        public Enumeration<String> getHeaderNames() {
+            Set<String> set = new HashSet<>(customHeaders.keySet());
+            Enumeration<String> e = super.getHeaderNames();
+            while (e.hasMoreElements()) {
+                set.add(e.nextElement());
+            }
+            return Collections.enumeration(set);
+        }
+
+        @Override
+        public Enumeration<String> getHeaders(String name) {
+            List<String> values = new ArrayList<>();
+
+            if(customHeaders.containsKey(name)){
+                values.add(customHeaders.get(name));
+            }
+
+            Enumeration<String> e = super.getHeaders(name);
+            while(e.hasMoreElements()){
+                values.add(e.nextElement());
+            }
+
+            return Collections.enumeration(values);
+        }
     }
 }
