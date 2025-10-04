a89624eeff1a84fd86972b3cbaa391ee38bfb298
khw7385 <khw7385@naver.com>
2025-10-04 15:56:05 +0900

    init: waitingroom 모듈 초기화

---
+++ backend/waitingroom/src/main/java/me/khw7385/waitingroom/waitingroom/WaitingroomApplication.java
@@ -0,0 +1,11 @@
+package me.khw7385.waitingroom.waitingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingroomApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingroomApplication.class, args);
+	}
+}
---
+++ backend/waitingroom/src/test/java/me/khw7385/waitingroom/waitingroom/WaitingroomApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khw7385.waitingroom.waitingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
